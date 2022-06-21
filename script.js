2ac22d764f0ec4818033e089a569c7d1590ce629
Niraj Gupta <nirajguptasde@gmail.com>
2022-06-22 03:29:30 +0530

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Welcome to the boilerplate');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+
+// prompt to select the media stream
+
+async function selectMediaStream(){
+  try {
+    // do this 
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    }
+  } catch (err) {
+    console.log('Woops, error here:', err);
+  }
+}  
+
+button.addEventListener('click', async () => {
+  // disable the button
+  button.disabled = true;
+  // start pINp
+  await videoElement.requestPictureInPicture();
+  // reset button 
+  button.disabled = false;
+});
+
+//On load
+selectMediaStream();
